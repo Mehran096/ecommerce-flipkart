bf4c6bb933f4185920ca82842d68b73058a5cb33
Mehran Hussain <mehranh91309@gmail.com>
2021-06-04 16:23:32 +0500

    new url added

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
-
+const baseUrl = "https://flipkartt-rest-server.herokuapp.com";
+//const baseUrl = "http://localhost:8080";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
