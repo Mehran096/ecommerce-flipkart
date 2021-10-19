1ecfd647b83af49694abe9a36ce38a59c6fb173a
Mehran Hussain <mehranh91309@gmail.com>
2021-10-20 01:51:58 +0500

    new line added

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkartt-rest-server.herokuapp.com";
+const baseUrl = "https://flipkart-clone-server-app.herokuapp.com";
 //const baseUrl = "http://localhost:8080";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
